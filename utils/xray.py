91dbdb033a7ad43935b928c1719be227b46e4b5b
johansundstain <johansundstain@outlook.com>
2026-07-31 16:39:50 +0700

    spx

--- utils/xray.py
+++ utils/xray.py
@@ -124,7 +124,7 @@ def create_link(user_id):
 	
 		base_url = (
 			f"vless://{uuid}@{ip}:443"
-			f"&security=reality"
+			f"?security=reality"
 			f"&sni={sni}"
 			f'&fp=chrome'
 			f"&pbk={pbk}"
