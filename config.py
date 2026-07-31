827476a7009a03f62517ca3e850b64d01fe3754d
johansundstain <johansundstain@outlook.com>
2026-07-31 15:22:33 +0700

    test

--- config.py
+++ config.py
@@ -2,14 +2,17 @@ import os
 from local_secrets import (
 	api_key,
 	admin_id,
-	owner_id
+	owner_id,
+	pbk,
+	server_name
 )
 
 API_KEY = api_key
 ADMIN_ID = admin_id
 OWNER_ID = owner_id
+PBK = pbk 
+SERVER_NAME  = server_name
+
 
 XRAY_CONF_PATH = "/usr/local/etc/xray/config.json"
-SERVER_NAME  = 'kz_xhttp'
 IP = None
-PBK = "nhel6cAx8AhDIn20wtm3hl2ALMqo_PgMPB5Y_1-23Gg"
