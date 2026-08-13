699112cec7962bee7367d880ab9cdcd3f52f911f
johansundstain <johansundstain@outlook.com>
2026-08-13 12:27:53 +0700

    new server

--- servers/servers.py
+++ servers/servers.py
@@ -10,8 +10,6 @@ class ServerInterface:
 		self._contries: List[APIServer] = [
 			APIServer(id="fi-1",emoji="🇫🇮", ip="185.254.158.230:8000", type='raw', api_key=API_TOKEN), 
 			APIServer(id="pl-1",emoji="🇵🇱", ip="45.43.137.116:8000", type='xhttp', api_key=API_TOKEN), 
-			#APIServer(id="kz-1",emoji="🇰🇿", ip="45.43.159.220:8000", type='xhttp', api_key=API_TOKEN), 
-			#APIServer(id="nl-1",emoji="🇳🇱", ip="2.26.134.157:8000", type='raw', api_key=API_TOKEN), 
 			APIServer(id="de-1",emoji="🇩🇪", ip="185.245.35.242:8000", type='xhttp', api_key=API_TOKEN), 
 			APIServer(id="none",emoji="🏴‍☠️", ip="0.0.0.0:8000", type='none', api_key=API_TOKEN), 
 		]
