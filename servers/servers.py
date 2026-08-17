2837d79de4f91485c550a1fdccb17c9fa2dd5f23
johansundstain <johansundstain@outlook.com>
2026-08-17 19:07:44 +0700

    new server

--- servers/servers.py
+++ servers/servers.py
@@ -11,6 +11,7 @@ class ServerInterface:
 			APIServer(id="fi-1",emoji="🇫🇮", ip="185.254.158.230:8000", type='raw', api_key=API_TOKEN), 
 			APIServer(id="pl-1",emoji="🇵🇱", ip="45.43.137.116:8000", type='xhttp', api_key=API_TOKEN), 
 			APIServer(id="de-1",emoji="🇩🇪", ip="185.245.35.242:8000", type='xhttp', api_key=API_TOKEN), 
+			APIServer(id="hk-1",emoji="🇭🇰", ip="176.98.181.174:8000", type='raw', api_key=API_TOKEN), 
 			APIServer(id="none",emoji="🏴‍☠️", ip="0.0.0.0:8000", type='none', api_key=API_TOKEN), 
 		]
 
