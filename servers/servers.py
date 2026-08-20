093264a23c808c791386995f8dcce65884b36857
johansundstain <johansundstain@outlook.com>
2026-08-20 11:10:49 +0700

    delete fi-1

--- servers/servers.py
+++ servers/servers.py
@@ -8,7 +8,6 @@ from config import API_TOKEN
 class ServerInterface:
 	def __init__(self):
 		self._contries: List[APIServer] = [
-			APIServer(id="fi-1",emoji="🇫🇮", ip="185.254.158.230:8000", type='raw', api_key=API_TOKEN), 
 			APIServer(id="pl-1",emoji="🇵🇱", ip="45.43.137.116:8000", type='xhttp', api_key=API_TOKEN), 
 			APIServer(id="de-1",emoji="🇩🇪", ip="185.245.35.242:8000", type='xhttp', api_key=API_TOKEN), 
 			APIServer(id="hk-1",emoji="🇭🇰", ip="176.98.181.174:8000", type='xhttp', api_key=API_TOKEN), 
