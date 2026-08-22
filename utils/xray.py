79d1be54ad24f6ec256a3bbac29fada71e0fc6c9
johansundstain <johansundstain@outlook.com>
2026-08-22 22:58:21 +0700

    Create base uart read\write functions

--- utils/xray.py
+++ utils/xray.py
@@ -129,7 +129,7 @@ def create_link(user_id):
 		f"?type={type}"
 		f"&security=reality"
 		f"&pbk={pbk}"
-		f'&fp=firefox'
+		f'&fp=chrome'
 		f"&sni={sni}"
 		f"&sid={sid}"
 		f"&path={encoded_path}"
